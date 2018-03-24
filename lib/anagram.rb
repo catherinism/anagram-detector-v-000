3c19ed5421f2c6dd179d4dcdd92d149a0b5ad751
Catherine Salazar <catcreates13@gmail.com>
2018-03-24 23:26:55 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram
+
+  attr_accessor :words
+
+  def initialize(word)
+    @words=word
+  end
+
+  def match(array)
+    anagram = []
+    anagram.detect do |word| word
+    end
+
+  end
+
+end
+
+#select correct anagrams from the list of anagrams
